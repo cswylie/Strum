6f996aa7f4c90f1dc45d88f64f3b62b30dab7b3b
Christian Wylie <christianswylie@gmail.com>
2025-08-19 18:16:20 -0700

    taking out node backend

--- strum_service/server.ts
+++
@@ -1,7 +0,0 @@
-import app from './app';
-const PORT = process.env.PORT || 3000;
-
-app.listen(PORT, () => {
-  console.log(`Server Running on port ${PORT}`);
-  console.log(`API Testing UI: http://localhost:${PORT}/api/v0/docs/`);
-});
--- strum_service/src/strum.index.ts
+++
@@ -1,9 +0,0 @@
-export interface Message {
-  message: string;
-  history: {question: string, answer: string}[];
-}
-
-export interface ResponseMessage {
-    history: {question: string, answer: string}[];
-    response: string;
-}
